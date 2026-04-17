e6b8d0dc65edfad7ce8f4e86db763d09c5af818b
kawsar9867 <kbosuniya@gmail.com>
2026-04-17 23:59:51 +0600

    status page update

--- src/components/NavBar/Status/Status.jsx
+++ src/components/NavBar/Status/Status.jsx
@@ -1,12 +1,14 @@
 import React, { useContext } from "react";
 import { Mycontext } from "../../API/Context";
+import { Legend, Pie, Tooltip } from "recharts";
+import { PieChart } from "lucide-react";
 
 function Status() {
-  const { friendData } = useContext(Mycontext);
+  const { card } = useContext(Mycontext);
 
-  const callCount = friendData.filter((f) => f.type === "Call").length;
-  const textCount = friendData.filter((f) => f.type === "Text").length;
-  const videoCount = friendData.filter((f) => f.type === "Video").length;
+  const callCount = card.filter((f) => f.type === "Call").length;
+  const textCount = card.filter((f) => f.type === "Text").length;
+  const videoCount = card.filter((f) => f.type === "Video").length;
 
   const data = [
     { name: "Call", value: callCount, fill: "#01859E" },
@@ -15,14 +17,14 @@ function Status() {
   ];
 
   return (
-    <div className="bg-[#F8FAFC] dark:bg-gray-900 ">
+    <div className="bg-[#F8FAFC]  ">
       <div className="max-w-285 mx-auto my-10 px-5">
-        <h1 className="text-3xl font-bold text-[#244D3F]  dark:text-gray-200 mb-5">
-          Friendship Analytics
+        <h1 className="text-3xl font-bold text-[#244D3F]   mb-5">
+           Analytics
         </h1>
 
-        <div className="bg-white dark:bg-gray-800 p-5 rounded-md mb-5">
-          <h2 className="text-2xl font-semibold text-[#244D3F] dark:text-gray-300">
+        <div className="bg-white  p-5 rounded-md mb-5">
+          <h2 className="text-2xl font-semibold text-[#244D3F] ">
             By Interaction Type
           </h2>
           <div className="flex justify-center">
